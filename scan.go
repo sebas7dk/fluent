e92ab720183ed6954d98e9dfe66f87f0c6c13e54
Sebastian de Kok <sebastian.dekok@sainsburys.co.uk>
2017-06-09 14:55:18 +0100

    Add tests to scanner

---
+++ scan.go
@@ -0,0 +1,140 @@
+package fluent
+
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+const scannerTag = "sql"
+
+type scanner struct {
+	value interface{}
+}
+
+func (sc *scanner) Scan(val interface{}) error {
+	switch val.(type) {
+	case int:
+		if v, ok := val.(int); ok {
+			val = int64(v)
+		}
+	case []byte:
+		// Strings and floats come back as []uint8
+		if v, ok := val.([]uint8); ok {
+			val = string(v)
+			if f, err := strconv.ParseFloat(val.(string), 64); err == nil {
+				val = f
+			}
+		}
+	}
+	sc.value = val
+	return nil
+}
+
+func scanStruct(s interface{}, vals map[string]interface{}) error {
+	valsLen := len(vals)
+	if valsLen == 0 {
+		return fmt.Errorf("The values map shouldn't be empty")
+	}
+
+	valOf := reflect.Indirect(reflect.ValueOf(s))
+	if valOf.Kind() != reflect.Struct {
+		return fmt.Errorf("The provided interface is not a struct")
+	}
+
+	for i := 0; i < valOf.Type().NumField(); i++ {
+		// Get the field
+		field := valOf.Field(i)
+
+		// Get the tags associated with the field
+		tag := valOf.Type().Field(i).Tag.Get(scannerTag)
+		if len(strings.TrimSpace(tag)) == 0 {
+			// Skip empty tags
+			continue
+		}
+		fieldName := valOf.Type().Field(i).Name
+
+		if !field.CanSet() {
+			return fmt.Errorf("Can't set the value for field: %s", fieldName)
+		}
+
+		if val := vals[tag]; val != nil {
+			if field.Type() != reflect.TypeOf(val) {
+				return fmt.Errorf("The field %s and the value are not of the same type", fieldName)
+			}
+
+			if err := setFieldValue(field, val); err != nil {
+				return fmt.Errorf("Field %s: %s", fieldName, err)
+			}
+		}
+	}
+
+	return nil
+}
+
+func setFieldValue(field reflect.Value, v interface{}) error {
+
+	switch field.Kind() {
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		val, ok := v.(int)
+		if !ok {
+			return fmt.Errorf("unable to set the integer value")
+		}
+		field.SetInt(int64(val))
+	case reflect.Float32, reflect.Float64:
+		val, ok := v.(float64)
+		if !ok {
+			return fmt.Errorf("unable to set the float value")
+		}
+		field.SetFloat(val)
+	case reflect.String:
+		val, ok := v.(string)
+		if !ok {
+			return fmt.Errorf("unable to set the string value")
+		}
+		field.SetString(val)
+	case reflect.Bool:
+		val, ok := v.(bool)
+		if !ok {
+			return fmt.Errorf("unable to set the bool value")
+		}
+		field.SetBool(val)
+	default:
+		field.Set(reflect.ValueOf(v))
+	}
+
+	return nil
+}
+
+func scanStructSlice(s interface{}, vals []map[string]interface{}) error {
+	if s == nil {
+		return fmt.Errorf("The slice shouldn't be empty")
+	}
+
+	if len(vals) == 0 {
+		return fmt.Errorf("The values map shouldn't be empty")
+	}
+	if reflect.TypeOf(s).Kind() != reflect.Ptr {
+		return fmt.Errorf("The provided type is not a pointer")
+	}
+
+	valOf := reflect.ValueOf(s).Elem()
+	if valOf.Kind() != reflect.Slice {
+		return fmt.Errorf("The provided value is not a slice")
+	}
+
+	// Loop through the map
+	for _, val := range vals {
+		// New slice pointer to write to
+		ptr := reflect.New(valOf.Type().Elem()).Interface()
+		if err := scanStruct(ptr, val); err != nil {
+			return err
+		}
+
+		ptrVal := reflect.ValueOf(ptr).Elem()
+		valOf.Set(reflect.Append(valOf, ptrVal))
+	}
+
+	return nil
+}
